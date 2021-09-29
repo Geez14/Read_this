eebb7cdc7bd0fcd13fecfa58b11f8a5a3b0f5505
Mxtylish007 <91541128+Mxtylish007@users.noreply.github.com>
2021-09-29 16:07:53 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,4 +1,9 @@
 print("Hello world")
-a= input(int())
-b= input(int())
-print("your answer is", a*b)
+Print("your first number")
+a= float(input())
+print("your second number")
+b= float(input())
+print("if we multiply, we get: ", a*b)
+print("Add: ", a+b)
+print("Division of first numb by second numb is ", a/b)
+
